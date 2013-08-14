c085a585ce1043f8d246fcaacc027d01adb2fbcd
choizhang <461261438@qq.com>
2013-08-14 20:50:00 +0800

    aaaaa

--- app/js/common/iFrequency.js
+++ app/js/common/iFrequency.js
@@ -3,15 +3,12 @@ define(function(require, exports, module) {
  * 页面刷新的频率，这个应该是主程序，因为刷新后要很多初始化操作
  * ====================== */
 	exports.getTime = function(floor, target_floor, tid){
-		var time;
-		if(floor < target_floor*0.5){
-			tid && clearInterval(tid));
+		var time = 0;
+		if(floor == target_floor*0.5){
 			time = 60 * 1000;
-		} else if(floor < target_floor*0.8){
+		} else if(floor == target_floor*0.8){
 			time = 30 * 1000;
 		} else if(floor < target_floor*0.9){
-			time = 10 * 1000;
-		} else {
 			time = 1000;
 		}
 		return time;
