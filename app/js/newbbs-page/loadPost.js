c75f5024a104cab3387c735ee7164de4db7d578e
weishai <masterkingr9@gmail.com>
2013-08-25 22:02:56 +0800

    update reader

--- app/js/newbbs-page/loadPost.js
+++ app/js/newbbs-page/loadPost.js
@@ -1,2 +1,36 @@
-// $('body').css('background', '#08c');
-alert('test well');
+console.log('join success')
+var siteUrl = location.origin
+  , postTitle
+  , msgExt = chrome.extension.connect({name: "postPage"})
+
+chrome.extension.onConnect.addListener(function(port) {
+  port.onMessage.addListener(function(msg) {
+    switch(msg.action){
+      case 'getPostList':
+        postTitle = getPostTitle()
+        console.log(postTitle)
+        msgExt.postMessage({action: 'getPostList', data: postTitle});
+        break;
+      case 'getPost':
+        
+      default:
+        break;
+    }
+  });
+});
+
+
+function getPostTitle() {
+  var targetAry = []
+    , tbody = document.querySelectorAll('.mainbox tbody')
+  for (var i = 0; i < tbody.length; i++) {
+    var postItem = {}
+    if(tbody[i].id.indexOf('normalthread') != -1){
+      var curA = tbody[i].querySelector('.tsubject a');
+      postItem.title = curA.innerText;
+      postItem.url = curA.href;
+      targetAry.push(postItem);
+    }
+  }
+  return targetAry;
+}
--- app/js/newbbs-page/main.js
+++ app/js/newbbs-page/main.js
@@ -15,29 +15,65 @@ define(function(require) {
     /* newbbsTab */
     $('#newbbsTab').tab();
 
-    container.render();
+   //  container.render();
 
-	  //点击添加监控后获取当前标签页的url和标题
-    document.querySelector('.icon-plus').addEventListener('click', function() {
-      chrome.tabs.getSelected(null,function(thisTab){
-              $('#tie-url').val(thisTab.url);
-              $('#tie-tabid').val(thisTab.id);
-              $('#tie-title').val(thisTab.title.substring(8));
-      });       
+	  // //点击添加监控后获取当前标签页的url和标题
+   //  document.querySelector('.icon-plus').addEventListener('click', function() {
+   //    chrome.tabs.getSelected(null,function(thisTab){
+   //            $('#tie-url').val(thisTab.url);
+   //            $('#tie-tabid').val(thisTab.id);
+   //            $('#tie-title').val(thisTab.title.substring(8));
+   //    });
+   //  });
+
+   //  //点击完成新增一个监控帖子的记录
+   //  document.querySelector('#submit-addForm').addEventListener('click', function() {
+   //  	container.add({
+   //        url: $('#tie-url').val(),
+   //        title: $('#tie-title').val(),
+   //        floor: $('#tie-floor').val(),
+   //        tabid: parseInt($('#tie-tabid').val()),
+   //        status: 1
+   //    })
+   //    //返回主页面并将新增的数据添加到主页面
+   //    $('.icon-undo').trigger('click');
+   //  });
+
+    //reader
+    chrome.tabs.executeScript(null, {file: "./js/newbbs-page/loadPost.js"});
+
+    //建立与页面content script的通讯接口
+    var pageExt
+    chrome.tabs.getSelected(null, function(tab) {
+      //console.log(tab.id)
+      pageExt = chrome.tabs.connect(tab.id, {name: 'postPage'});
     });
 
-    //点击完成新增一个监控帖子的记录
-    document.querySelector('#submit-addForm').addEventListener('click', function() {
-    	container.add({
-          url: $('#tie-url').val(),
-          title: $('#tie-title').val(),
-          floor: $('#tie-floor').val(),
-          tabid: parseInt($('#tie-tabid').val()),
-          status: 1
-      })
-      //返回主页面并将新增的数据添加到主页面
-      $('.icon-undo').trigger('click');
+    chrome.extension.onConnect.addListener(function(port) {
+      //console.assert(port.action == "loadPost");
+      port.onMessage.addListener(function(msg) {
+        if (msg.action == "getPostList"){
+          //console.log('msg good')
+          var itemsHtml = '';
+          for(var curItem = 0; curItem < 10; curItem++){
+            console.log(curItem)
+            itemsHtml += '<div class="task-item"><a href="'+msg.data[curItem].url+'"><h4 class="item-title">' +msg.data[curItem].title+ '</h4></a></div>'
+          }
+          //console.log(itemsHtml)
+          $('#tab-reader .luckyBox-main .task-list').html(itemsHtml);
+        }
+      });
     });
-  });
 
+    $('#getPostBtn').on('click', function () {
+      pageExt.postMessage({action: 'getPostList'});
+    });
+
+    $('#tab-reader .luckyBox-main').on('click', '.task-item > a', function () {
+      var curPostUrl = $(this).attr('href');
+      chrome.tabs.create({url}, function callback)
+      pageExt.postMessage({action: 'getPost', posturl: curPostUrl});
+    });
+
+  });//end jQuery
 });
