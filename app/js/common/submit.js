01f6c0fd47468d640e10dae8add5dceeecaa48e7
weishai <masterkingr9@gmail.com>
2013-12-24 14:53:29 +0800

    u

--- app/js/common/submit.js
+++ app/js/common/submit.js
@@ -1,12 +1,8 @@
 var list = [
-  '活动',
-  '支持支持',
-  '活动很给力',
-  '我想中奖',
-  '沙发沙发',
-  '三国很好玩，期待期待',
-  '惊世公测，绝不错过',
-  '烽火戏诸侯'
+  'Q-Learning赞一个！',
+  '感谢Q-Learning的美好课程',
+  'Q-Learning越做越好！',
+  '活动不错'
 ]
 //将输入框里面注入内容
 document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("div")[0].innerHTML = list[Math.floor(Math.random()*list.length)];
