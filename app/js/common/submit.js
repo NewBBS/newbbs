c35fdae05984f96bcae69cd5138f8137410dad11
weishai <masterkingr9@gmail.com>
2014-01-28 17:12:10 +0800

    update

--- app/js/common/submit.js
+++ app/js/common/submit.js
@@ -5,6 +5,7 @@ var list = [
   '活动不错'
 ]
 //将输入框里面注入内容
-document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("div")[0].innerHTML = list[Math.floor(Math.random()*list.length)];
+document.getElementsByTagName("iframe")[0].contentDocument.querySelector('#tinymce p').innerHTML = list[Math.floor(Math.random()*list.length)];
+console.log('submit');
 //点击发表回复按钮
-document.querySelector('#submit_button').click();
+//document.querySelector('#submit_button').click();
